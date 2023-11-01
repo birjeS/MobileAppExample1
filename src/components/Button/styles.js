3fe4cf912a8abcda556bc4f26e7ee4e5f56ba497
birjeS <birje.solatte@voco.ee>
2023-11-01 09:12:17 +0200

    Create Splash screen

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles=StyleSheet.create({
+    container:{
+        backgroundColor:'#4f63ac',
+        paddingHorizontal:8,
+        paddingVertical:20,
+        borderRadius:8,
+        width:'100%'
+
+
+    },
+    title:{
+        fontSize:16,
+        fontWeight:"bold",
+        textAlign:"center",
+        color:'#fff'
+}
+})
